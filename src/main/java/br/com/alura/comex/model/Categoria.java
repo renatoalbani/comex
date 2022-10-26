1afc76bc9ab7fff0de056647a9aa43104e54bfc8
Renato Albani dos Santos <renato.santos@furukawaelectric.com>
2022-10-26 00:05:45 -0300

    Initializing project structure

---
+++ src/main/java/br/com/alura/comex/model/Categoria.java
@@ -0,0 +1,68 @@
+package br.com.alura.comex.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusCategoria status = StatusCategoria.ATIVA;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Produto> produtos = new ArrayList<>();
+
+    public Categoria() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public StatusCategoria getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusCategoria status) {
+        this.status = status;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public void adicionaProduto(Produto produto){
+        produto.setCategoria(this);
+        this.produtos.add(produto);
+    }
+
+}
