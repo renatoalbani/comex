d699db2c0a7ebc135be00b196d7b25adbb9c30c7
Renato Albani dos Santos <renato.santos@furukawaelectric.com>
2022-11-13 23:14:03 -0300

    [Alura-Semana 1] Endpoint relatorio de pedidos por categoria
    - Relatorio de pedidos por categoria usando projecao

---
+++ src/main/java/br/com/alura/comex/model/CategoriaPedidosProjecao.java
@@ -0,0 +1,9 @@
+package br.com.alura.comex.model;
+
+public interface CategoriaPedidosProjecao {
+
+    String getNome();
+    Long getProdutos();
+    Integer getQuantidade();
+
+}
--- src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
+++ src/main/java/br/com/alura/comex/repository/CategoriaRepository.java
@@ -1,7 +1,21 @@
 package br.com.alura.comex.repository;
 
 import br.com.alura.comex.model.Categoria;
+import br.com.alura.comex.model.CategoriaPedidosProjecao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    @Query(value = "SELECT c.nome, " +
+            "COUNT(DISTINCT p.id) as produtos, " +
+            "SUM(ip.quantidade) as quantidade  " +
+            "FROM categorias c " +
+            "JOIN produtos p on p.categoria_id = c.id " +
+            "JOIN itens_pedido ip on ip.produto_id = p.id " +
+            "GROUP BY c.id" , nativeQuery = true)
+    List<CategoriaPedidosProjecao> findCategoriaPedidos();
+
 }
