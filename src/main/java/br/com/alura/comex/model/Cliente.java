b92abb7086e6d74b39fd95d9a30ec917f8c5c043
Renato Albani dos Santos <renato.santos@furukawaelectric.com>
2022-11-14 00:47:33 -0300

    [Alura-Semana 2] Mapear entidade usuario
    - Adicionada entidade Usuario e mapeado em Cliente

--- src/main/java/br/com/alura/comex/model/Cliente.java
+++ src/main/java/br/com/alura/comex/model/Cliente.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -39,6 +40,9 @@ public class Cliente {
 
 	@Column(nullable = false)
 	private String estado;
+
+	@OneToOne
+	private Usuario usuario;
 
 	public Cliente() {
 	}
---
+++ src/main/java/br/com/alura/comex/model/Usuario.java
@@ -0,0 +1,44 @@
+package br.com.alura.comex.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
